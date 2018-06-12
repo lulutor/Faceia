e9a1b810df154782d7c0fb7b4d26474af3c2b827
lucas <lucas.nandan@gmail.com>
2018-06-12 16:59:06 +0200

    recognition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,87 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+import Particles from 'react-particles-js';
+import Clarifai  from 'clarifai';
+import Signin from './components/Signin/Signin';
+
+const app = new Clarifai.App({
+  apiKey: 'c5afb6f210fa4f69ace37e5575979136'
+ });
+ 
+const particlesOptions = {
+  particles: {
+   number: {
+    value: 70,
+    density: {
+      enable: true,
+      value_area: 800,
+    }
+   }
+  }
+}
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className="particles"
+          params={particlesOptions}
+        />
         <Navigation />
-         <Logo />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <Signin />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
